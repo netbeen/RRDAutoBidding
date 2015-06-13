8574c707a87a14c5a73e8c96acfd34fd6b95e967
NetBeen <netbeen.cn@gmail.com>
2015-06-13 22:30:42 +0800

    Add comment, and set the maximin page to 99

--- getInfo.py
+++ getInfo.py
@@ -4,13 +4,17 @@
 import urllib2
 import json
 
+#抓取表单函数
 def getInfo():
+        #URL前缀
         loanListUrlPrefix = 'http://www.renrendai.com/lend/loanList!json.action?pageIndex='
         transferListUrlPreFix = 'http://www.renrendai.com/transfer/transferList!json.action?pageIndex='
         
+        #声明汇总表单
         usefulInfo = []
         
-        for pageIndex in range(3):
+        #抓取散标投资信息
+        for pageIndex in range(99):
                 webContent = urllib2.urlopen(loanListUrlPrefix+str(pageIndex+1)).read()
                 if webContent[0] == '<':
                         return -1;
@@ -22,7 +26,8 @@ def getInfo():
                 if jsonString["data"]["loans"][19]["finishedRatio"] == 100:
                         break;
         
-        for pageIndex in range(3):
+        #抓取债券转让信息
+        for pageIndex in range(99):
                 webContent = urllib2.urlopen(transferListUrlPreFix+str(pageIndex+1)).read()
                 if webContent[0] == '<':
                         return -1;
@@ -34,6 +39,7 @@ def getInfo():
                 if jsonString["data"]["transferList"][19]["share"] == '0':
                         break;
         
+        #去除无效信息
         for elem in reversed(range(len(usefulInfo))):
                 if usefulInfo[elem]['isTransfer']==True and usefulInfo[elem]['share']=='0':
                         del usefulInfo[elem]
@@ -41,7 +47,7 @@ def getInfo():
                 if usefulInfo[elem]['isTransfer']==False and usefulInfo[elem]['finishedRatio']==100:
                         del usefulInfo[elem]    
         
-        
+        #按照利率进行排序
         usefulInfo.sort( key=lambda elemUsefulInfo : elemUsefulInfo["interest"], reverse=True)
         
         return usefulInfo
