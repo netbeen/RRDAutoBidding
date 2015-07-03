23bd30796f4c7648b6e119cc0c63bd7ca6eb2d79
NetBeen <netbeen.cn@gmail.com>
2015-07-03 22:02:23 +0800

    添加一个%,更加方便看

--- main.py
+++ main.py
@@ -11,17 +11,19 @@ if __name__ == '__main__':
                 usefulInfo = getInfo();
                 if usefulInfo == -1:
                         failedCount=failedCount+1
+                        print time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))+"\t成功："+str(successCount)+"\t失败："+str(failedCount)
+                        time.sleep(60)
                         continue
                 else:
                         successCount=successCount+1
                         os.system('clear')
-                        print time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))+"\t成功："+str(successCount)+"\t失败："+str(failedCount)
+                        print time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))+"\t债权总数："+str(len(usefulInfo))+"\t成功："+str(successCount)+"\t失败："+str(failedCount)
                         print '----------------------------------------------------------------------'
-                        for element in  usefulInfo[0:20]:
+                        for element in  usefulInfo[0:30]:
                                 if element['isTransfer']==True:
                                         print str(element["interest"]) + "\t" + str(element["borrowerLevel"])+ "\t" + str(element["displayLoanType"])+ "\t" + str(element["share"]) + "\t" + "转"
                                 else:
-                                        print str(element["interest"]) + "\t" + str(element["borrowerLevel"])+ "\t" + str(element["displayLoanType"])+ "\t" + str(element["finishedRatio"])
-                        time.sleep(1)
+                                        print str(element["interest"]) + "\t" + str(element["borrowerLevel"])+ "\t" + str(element["displayLoanType"])+ "\t" + str(element["finishedRatio"]) + " %"
+                        time.sleep(len(usefulInfo)/20+1)
                  
                 
